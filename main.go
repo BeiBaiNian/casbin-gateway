b3e95a863f4beef5bd1029a932af748fb7ebf3d2
Yang Luo <hsluoyz@gmail.com>
2026-08-19 01:39:06 +0800

    fix: support Vite build layout in CDN upload

--- main.go
+++ main.go
@@ -74,8 +74,6 @@ func main() {
 		AllowCredentials: true,
 	}))
 
-	//beego.DelStaticPath("/static")
-	beego.SetStaticPath("/static", "web/build/static")
 	// https://studygolang.com/articles/2303
 	beego.InsertFilter("/", beego.BeforeRouter, routers.TransparentStatic) // must has this for default page
 	beego.InsertFilter("/*", beego.BeforeRouter, routers.TransparentStatic)
